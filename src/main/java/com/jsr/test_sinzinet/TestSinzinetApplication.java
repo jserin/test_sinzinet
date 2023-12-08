8dd57f2763a6f74a05993e7a2e14b84870b3bf42
Jserin <jsr3643@naver.com>
2023-12-09 01:25:03 +0900

    first commit

---
+++ src/main/java/com/jsr/test_sinzinet/TestSinzinetApplication.java
@@ -0,0 +1,13 @@
+package com.jsr.test_sinzinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSinzinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSinzinetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsr/test_sinzinet/TestSinzinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsr.test_sinzinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSinzinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
